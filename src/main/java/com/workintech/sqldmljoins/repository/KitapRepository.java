369920c9eccfdde3243a0a25b9b0d98db1e5b509
gularal <gul.043433@gmail.com>
2025-04-16 00:11:17 +0300

    commit

--- src/main/java/com/workintech/sqldmljoins/repository/KitapRepository.java
+++ src/main/java/com/workintech/sqldmljoins/repository/KitapRepository.java
@@ -9,12 +9,12 @@ import java.util.List;
 public interface KitapRepository extends JpaRepository<Kitap, Long> {
 
     //Dram ve Hikaye türündeki kitapları listeleyin. JOIN kullanmadan yapın.
-    String QUESTION_1 = "";
+    String QUESTION_1 = "SELECT * FROM kitap WHERE turno IN (SELECT turno FROM tur WHERE turadi IN ('Dram', 'Hikaye'))";
     @Query(value = QUESTION_1, nativeQuery = true)
     List<Kitap> findBooks();
 
 
-    String QUESTION_10 = "";
+    String QUESTION_10 = "SELECT O.ad, o.soyad, COUNT(i.islemno) AS kitap_sayisi FROM ogrenci o LEFT JOIN islem i ON p.ogrnno GROUP BY o.ad, o.soyad";
     @Query(value = QUESTION_10, nativeQuery = true)
     Double findAvgPointOfBooks();
 
--- src/main/java/com/workintech/sqldmljoins/repository/OgrenciRepository.java
+++ src/main/java/com/workintech/sqldmljoins/repository/OgrenciRepository.java
@@ -10,43 +10,43 @@ public interface OgrenciRepository extends JpaRepository<Ogrenci, Long> {
 
 
     //Kitap alan öğrencilerin öğrenci bilgilerini listeleyin..
-    String QUESTION_2 = "";
+    String QUESTION_2 = "SELECT o.* FROM ogrenci o WHERE o.ogrno IN (SELECT DISTINCT i.ogrno FROM islem i)";
     @Query(value = QUESTION_2, nativeQuery = true)
     List<Ogrenci> findStudentsWithBook();
 
 
     //Kitap almayan öğrencileri listeleyin.
-    String QUESTION_3 = "";
+    String QUESTION_3 = "SELECT * FROM ogrenci WHERE ogrnno NOT IN(SELECT DISTINCN ogrno FROM islem i)";
     @Query(value = QUESTION_3, nativeQuery = true)
     List<Ogrenci> findStudentsWithNoBook();
 
     //10A veya 10B sınıfındaki öğrencileri sınıf ve okuduğu kitap sayısını getirin.
-    String QUESTION_4 = "";
+    String QUESTION_4 = "SELECT o.sinif, COUNT(i.islemno) AS kitap_sayisi FROM ogrenci o LEFT JOIN i ON o.ogrno = i.ogrno = i.ogrno WHERE o.sinif IN('10A', '10B') GROUP BY o.sinif";
     @Query(value = QUESTION_4, nativeQuery = true)
     List<KitapCount> findClassesWithBookCount();
 
     //Öğrenci tablosundaki öğrenci sayısını gösterin
-    String QUESTION_5 = "";
+    String QUESTION_5 = "SELECT COUNT (*) FROM ogrenci";
     @Query(value = QUESTION_5, nativeQuery = true)
     Integer findStudentCount();
 
     //Öğrenci tablosunda kaç farklı isimde öğrenci olduğunu listeleyiniz.
-    String QUESTION_6 = "";
+    String QUESTION_6 = "SELECT COUNT (DISTINCT ad) FROM ogrenci";
     @Query(value = QUESTION_6, nativeQuery = true)
     Integer findUniqueStudentNameCount();
 
     //--İsme göre öğrenci sayılarının adedini bulunuz.
     //--Ali: 2, Mehmet: 3
-    String QUESTION_7 = "";
+    String QUESTION_7 = "SELECT ad COUNT(*) AS sayi FROM ogrenci GROUP BY ad";
     @Query(value = QUESTION_7, nativeQuery = true)
     List<StudentNameCount> findStudentNameCount();
 
 
-    String QUESTION_8 = "";
+    String QUESTION_8 = "SELECT sinif, COUNT(*) AS ogrenci_sayisi GROUP BY sinif";
     @Query(value = QUESTION_8, nativeQuery = true)
     List<StudentClassCount> findStudentClassCount();
 
-    String QUESTION_9 = "";
+    String QUESTION_9 = "SELECT o.ad, o.soyad, COUNT(i.islemno)  AS kitap_sayisi  FROM ogrenci o LEFT JOİN islem i ON o.ogrnno= i. ogrno GROUP BY o.ad, o.soyad";
     @Query(value = QUESTION_9, nativeQuery = true)
     List<StudentNameSurnameCount> findStudentNameSurnameCount();
 }
